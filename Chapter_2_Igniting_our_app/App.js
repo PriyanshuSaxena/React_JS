da1a86e2f4883e352a2157b3e9aa7289e6e8c37b
Priyanshu Saxena <priyanshu.s@codestoresolutions.com>
2023-07-20 23:53:27 +0530

    Chapter 2 Igniting our App

--- Chapter_2_Igniting_our_app/App.js
+++ Chapter_2_Igniting_our_app/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement("div", {}, [
   React.createElement("div", {}, [
     React.createElement("h1", {}, "h1 from child 1"),
